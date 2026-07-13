c0f201adc36f206321df78e1a617ac25dbc9363a
pyiron-runner <pyiron@mpie.de>
2026-07-13 19:00:18 +0000

    Format black

--- src/lammpsparser/_version.py
+++ src/lammpsparser/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev2+g6d86b2f9f.d20260713'
-__version_tuple__ = version_tuple = (0, 1, 'dev2', 'g6d86b2f9f.d20260713')
+__version__ = version = "0.1.dev2+g6d86b2f9f.d20260713"
+__version_tuple__ = version_tuple = (0, 1, "dev2", "g6d86b2f9f.d20260713")
 
 __commit_id__ = commit_id = None
